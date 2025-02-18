fecbcd74341bf9ab156ab31313a43e43c25517a2
eljapo22 <jhnapo2213@gmail.com>
2025-02-18 00:23:24 -0600

    Fix app structure for Streamlit Cloud deployment

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ setup(
     name="transformer_analysis",
     version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        "streamlit",
-        "plotly",
-        "pandas",
-        "duckdb",
-        "watchdog",
-        "pygments"
+        "streamlit>=1.24.0",
+        "duckdb>=0.8.1",
+        "pandas>=2.0.3",
+        "plotly>=5.15.0",
+        "numpy>=1.24.3",
+        "google-auth-oauthlib>=1.0.0",
+        "google-auth>=2.22.0",
+        "google-api-python-client>=2.95.0",
+        "python-dotenv>=1.0.0"
     ]
 )
